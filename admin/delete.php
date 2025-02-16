232a252b33e0099e4c241210d5038a9bb027c360
Akshay <kokaneakshay17@gmail.com>
2025-02-16 18:10:43 +0530

    Initial Admin Commited

---
+++ admin/delete.php
@@ -0,0 +1,34 @@
+<?php
+
+$servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "alumini";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Check if the delete button is clicked and if the ID is set in the request
+if (isset($_POST['delete']) && isset($_POST['id'])) {
+    $id = $_POST['id'];
+
+    // SQL query to delete the record with the specified ID
+    $sql = "DELETE FROM data WHERE id='$id'";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Record deleted successfully";
+    } else {
+        echo "Error deleting record: " . $conn->error;
+    }
+}
+
+$conn->close();
+
+// Redirect back to the display.php page
+header("location: display.php");
+?>
